832cec11602141b162806aa596b2e76c3c40c09c
然Y <inie0722@gmail.com>
2019-12-27 18:40:28 +0800

    queue适配器

---
+++ include/CTL_queue.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#include "CTL_container.h"
+#include "CTL_allocator.h"
+
+typedef struct
+{
+    CTL_container container;
+    CTL_functions *functions;
+
+}CTL_queue;
+
+void CTL_queue_new(CTL_queue *handle, CTL_functions *functions, size_t buf_size, size_t T_size);
+
+void CTL_queue_delete(CTL_queue *handle);
+
+void *CTL_queue_front(const CTL_queue *handle);
+
+void *CTL_queue_back(const CTL_queue *handle);
+
+void CTL_queue_push(CTL_queue *handle, void *data);
+
+void CTL_queue_pop(CTL_queue *handle);
+
+size_t CTL_queue_size(const CTL_queue *handle);
+
+bool CTL_queue_empty(const CTL_queue *handle);
