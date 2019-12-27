52afa708fdcb8e461a1683ee28230c2093e423f7
然Y <inie0722@gmail.com>
2019-12-27 20:19:24 +0800

    stack适配器

---
+++ test/stack.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <stack>
+#include <algorithm>
+#include <time.h>
+
+extern "C"
+{
+#include "CTL_stack.h"
+}
+
+using namespace std;
+
+stack<int> stl;
+CTL_stack ctl;
+
+void test_new()
+{
+    srand((unsigned)time(NULL));
+
+    CTL_stack_new(&ctl, CTL_deque_functions(), 18, sizeof(int));
+}
+
+TEST(Modifiers, push)
+{
+    for (int i = 0; i < 200; i++)
+    {
+        stl.push(i);
+        CTL_stack_push(&ctl, &i);
+        ASSERT_TRUE(stl.top() == *(int *)CTL_stack_top(&ctl));
+    }
+}
+
+
+TEST(Modifiers, pop)
+{
+    for (int i = 0; i < 199; i++)
+    {
+        stl.pop();
+        CTL_stack_pop(&ctl);
+        ASSERT_TRUE(stl.top() == *(int *)CTL_stack_top(&ctl));
+    }
+}
+
+TEST(Capacity, size)
+{
+    ASSERT_TRUE(stl.size() == CTL_stack_size(&ctl));
+}
+
+TEST(Capacity, empty)
+{
+    ASSERT_TRUE(stl.empty() == CTL_stack_empty(&ctl));
+}
+
+TEST(allocator, delete)
+{
+    CTL_stack_delete(&ctl);
+    ASSERT_TRUE(CTL_debug_mem_size == 0);
+    ASSERT_TRUE(CTL_debug_mem == 0);
+}
+
+int main(int argc, char **argv)
+{
+    test_new();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
