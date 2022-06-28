b76e72a18e7ac38447ac08771600ddb1d3e29239
然Y <inie0722@gmail.com>
2022-06-28 13:24:59 +0800

    修复 aba ptr 无法使用 问题 并增加 static_assert

--- include/CTL/lockfree/aba_pointer.h
+++ include/CTL/lockfree/aba_pointer.h
@@ -4,23 +4,36 @@
 
 #include "CTL/compat/atomic.h"
 
+#if defined __cplusplus
+static_assert(sizeof(void *) == 8, "only support 64-bit");
+#else
+_Static_assert(sizeof(void *) == 8, "only support 64-bit");
+#endif
+
 #define __CTL_ABA_COUNT_MASK (0XFFFFull << 48)
 #define __CTL_ABA_POINTER_MASK (~__CTL_ABA_COUNT_MASK)
 
 typedef uint64_t CTL_aba_pointer;
+
+inline CTL_aba_pointer CTL_aba_pointer_make(void *ptr)
+{
+    uint64_t new_ptr = (0ull << 48) + ((uintptr_t)ptr & __CTL_ABA_POINTER_MASK);
+
+    return new_ptr;
+}
 
 inline void *CTL_aba_pointer_get(CTL_aba_pointer ptr)
 {
     return (void *)(ptr & __CTL_ABA_POINTER_MASK);
 }
 
-#if defined _MSC_VER && defined __STDC_NO_ATOMICS__ && __STDC_VERSION__ >= 201112L
-inline CTL_aba_pointer CTL_aba_pointer_atomic_load(const _Atomic CTL_aba_pointer *obj, memory_order order = memory_order_seq_cst)
+#if defined __STDC_VERSION__
+inline CTL_aba_pointer CTL_aba_pointer_atomic_load(const _Atomic CTL_aba_pointer *obj, memory_order order)
 {
     return atomic_load_explicit(obj, order);
 }
 
-inline void CTL_aba_pointer_atomic_store(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer desired, memory_order order = memory_order_seq_cst)
+inline void CTL_aba_pointer_atomic_store(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer desired, memory_order order)
 {
     uint16_t count = ((desired & __CTL_ABA_COUNT_MASK) >> 48) + 1;
     uint64_t new_ptr = ((uint64_t)count << 48) + (desired & __CTL_ABA_POINTER_MASK);
@@ -28,7 +41,7 @@ inline void CTL_aba_pointer_atomic_store(_Atomic CTL_aba_pointer *obj, CTL_aba_p
     atomic_store_explicit(obj, new_ptr, order);
 }
 
-inline CTL_aba_pointer CTL_aba_pointer_atomic_exchange(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer desired, memory_order order = memory_order_seq_cst)
+inline CTL_aba_pointer CTL_aba_pointer_atomic_exchange(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer desired, memory_order order)
 {
     uint16_t count = ((desired & __CTL_ABA_COUNT_MASK) >> 48) + 1;
     uint64_t new_ptr = ((uint64_t)count << 48) + (desired & __CTL_ABA_POINTER_MASK);
@@ -36,30 +49,30 @@ inline CTL_aba_pointer CTL_aba_pointer_atomic_exchange(_Atomic CTL_aba_pointer *
     return atomic_exchange_explicit(obj, new_ptr, order);
 }
 
-inline bool CTL_aba_pointer_atomic_weak(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer *expected, CTL_aba_pointer desired, memory_order order = memory_order_seq_cst)
+inline bool CTL_aba_pointer_atomic_weak(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer *expected, CTL_aba_pointer desired, memory_order order)
 {
     uint16_t count = ((desired & __CTL_ABA_COUNT_MASK) >> 48) + 1;
     uint64_t new_ptr = ((uint64_t)count << 48) + (desired & __CTL_ABA_POINTER_MASK);
 
-    return atomic_compare_exchange_weak_explicit(obj, &expected, new_ptr, order);
+    return atomic_compare_exchange_weak_explicit(obj, expected, new_ptr, order, order);
 }
 
-inline bool CTL_aba_pointer_atomic_strong(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer *expected, CTL_aba_pointer desired, memory_order order = memory_order_seq_cst)
+inline bool CTL_aba_pointer_atomic_strong(_Atomic CTL_aba_pointer *obj, CTL_aba_pointer *expected, CTL_aba_pointer desired, memory_order order)
 {
     uint16_t count = ((desired & __CTL_ABA_COUNT_MASK) >> 48) + 1;
     uint64_t new_ptr = ((uint64_t)count << 48) + (desired & __CTL_ABA_POINTER_MASK);
 
-    return atomic_compare_exchange_strong_explicit(obj, &expected, new_ptr, order);
+    return atomic_compare_exchange_strong_explicit(obj, expected, new_ptr, order, order);
 }
 
-inline CTL_aba_pointer CTL_aba_pointer_atomic_fetch_add(_Atomic CTL_aba_pointer *obj, ptrdiff_t arg, memory_order order = memory_order_seq_cst)
+inline CTL_aba_pointer CTL_aba_pointer_atomic_fetch_add(_Atomic CTL_aba_pointer *obj, ptrdiff_t arg, memory_order order)
 {
     uint64_t des = (1ull << 48) + (arg & __CTL_ABA_POINTER_MASK);
 
     return atomic_fetch_add_explicit(obj, des, order);
 }
 
-inline CTL_aba_pointer CTL_aba_pointer_atomic_fetch_sub(_Atomic CTL_aba_pointer *obj, ptrdiff_t arg, memory_order order = memory_order_seq_cst)
+inline CTL_aba_pointer CTL_aba_pointer_atomic_fetch_sub(_Atomic CTL_aba_pointer *obj, ptrdiff_t arg, memory_order order)
 {
     uint64_t des = (1ull << 48) + (arg & __CTL_ABA_POINTER_MASK);
 
